b412fefb1c2abd456d7a5c794551383313fce124
irshad3786 <irshad9182278505@gmail.com>
2025-05-16 22:59:51 +0530

    backend and frontend changes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Home from './pages/Home'
 import Signin from './pages/Signin'
 import CreateAccount from './pages/CreatAccount'
+import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import AddProject from './pages/AddProject'
 import AllProjects from './pages/AllProjects'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const Router = createBrowserRouter([
+    {
+      path:'/',
+      element : <Home/>
+    },
+    {
+      path:'/Signin',
+      element : <Signin/>
+    },
+    {
+      path:'/CreateAccount',
+      element : <CreateAccount/>
+    },
+    {
+      path:'/Dashboard',
+      element : <Dashboard/>
+    },
+    {
+      path:'/AddProject',
+      element : <AddProject/>
+    },
+    {
+      path:'/AllProjects',
+      element : <AllProjects/>
+    },
 
+  ])
   return (
-    <>
-      <div>
-       <AllProjects/>
-      </div>
-       
-    </>
-  )
+    <div>
+      <RouterProvider router={Router}/>
+    </div>
+  );
 }
 
 export default App
